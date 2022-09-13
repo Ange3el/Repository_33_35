7af5ed3e5a3869be66c1bdcb6dd5d1671e43a5a9
Shepeliuk-Petro <petroshepeliuk@gmail.com>
2022-09-13 11:25:07 +0300

    Create lab4.py

---
+++ Lab/Shepeliuk/lab4.py
@@ -0,0 +1,13 @@
+# A = {3, 5, 11, 7, 4, -3}
+# B = {11, 5, 8, 1, 10, 7}
+# commonEl = A & B
+# differentEl = A - B
+# association = A | B
+# print(commonEl)
+# print(differentEl)
+# print(association)
+
+a = "a14b6fh"
+list = set(a)
+print(list)
+
