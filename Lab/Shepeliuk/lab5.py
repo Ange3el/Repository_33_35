77984c5d233ec3927f6f1947d77b0a15b12b9a73
Shepeliuk-Petro <petroshepeliuk@gmail.com>
2022-09-14 23:08:43 +0300

    Create lab5.py

---
+++ Lab/Shepeliuk/lab5.py
@@ -0,0 +1,33 @@
+#Task 1
+def maxList(list):
+    return max(list)
+list = [3,4,2,3,4,23,4,2,]
+print(maxList(list))
+
+# Task 2
+# def lenString(str):
+#     Len=len(str)
+#     return Len
+# print(lenString('ello'))
+
+
+#Task 3
+# def number(a,b):
+#     if(b>=0):
+#         return a**b
+#     else:
+#         return None
+# print(number(2,8))
+
+#Task 4
+# def lineOfNumber(n):
+#     i=1
+#     while i<=n:
+#         print(i)
+#         i=i+1
+# lineOfNumber(10)
+
+#Task 5
+# def rectangle(a,b):
+#     return a*b
+# print(rectangle(10,5))
