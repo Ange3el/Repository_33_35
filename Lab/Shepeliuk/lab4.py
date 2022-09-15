23fa4ba66f80f439cf7d4f3cc3dd65770979be84
Shepeliuk-Petro <petroshepeliuk@gmail.com>
2022-09-15 11:28:33 +0300

    lab

--- Lab/Shepeliuk/lab4.py
+++ Lab/Shepeliuk/lab4.py
@@ -1,3 +1,4 @@
+#Task 1-3
 # A = {3, 5, 11, 7, 4, -3}
 # B = {11, 5, 8, 1, 10, 7}
 # commonEl = A & B
@@ -7,7 +8,17 @@
 # print(differentEl)
 # print(association)
 
-a = "a14b6fh"
-list = set(a)
-print(list)
+#Task 4
+# arr ="a14b6fa"
+# setarr = set(arr)
+# if len(arr) == len(setarr):
+#     print("Всі елементи унікальні")
+# else:
+#     print("Є одинакові")
 
+#Task 5
+from ast import operator
+from collections import Counter
+dict = { 1:'Mon', 2:'Tue', 3:'Wed', 4:'Thu', 5:'Sat', 6:'Sat', 7:'Sun' }
+values = list(dict.values())
+count = operator.countOf(values, )
