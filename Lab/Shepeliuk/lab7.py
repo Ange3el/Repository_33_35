604593aa75d16cf8e7137434001f6b510f1a620b
Shepeliuk-Petro <petroshepeliuk@gmail.com>
2022-10-04 10:19:47 +0300

    commit

---
+++ Lab/Shepeliuk/lab7.py
@@ -0,0 +1,25 @@
+#Task 1
+# import random
+# import string
+# def random_char(y):
+#     return ' '.join(random.choice(string.ascii_letters) for x in range(y))
+# str = (random_char(25))
+# file = open('e:\\Lab\\Repository_33_35\\Lab\\Shepeliuk\\random_string.txt','w')
+# str = file.write(str)
+# file.close()
+
+#Task 2
+import random
+users = [
+    ['Артур',random.choice(range(1,99)) ], 
+    ['Кейт',random.choice(range(1,99))], 
+    ['Аліса',random.choice(range(1,99))],
+    ['Майк',random.choice(range(1,99))],
+] 
+newUsers = dict(users)
+params = {
+    'data': newUsers,
+    'created_at':'',
+}
+print(params)
+
